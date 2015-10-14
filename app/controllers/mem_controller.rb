10fb58f5576f2d1c76fc9442789b89a58002503c
hxh1246996371 <1246996371@qq.com>
2015-10-14 14:35:26 +0800

    mem login bug

--- app/controllers/mem_controller.rb
+++ app/controllers/mem_controller.rb
@@ -16,41 +16,45 @@ class MemController < ApplicationController
     if _mauth.nil?
       _mem = current_mem
       if !_mem
-        _raw_info = _data['extra']['raw_info']
-        _photo = ''
-        if _provider == 'github'
-          _photo =_raw_info['avatar_url']
-        end
-
-        #require 'uuidtools'   
-        #_filename =  UUIDTools::UUID.timestamp_create.to_s + '.jpg'
-        #upload_remote(_photo,_filename,'mem') 
-        _mem = Mem.create({
-          :nc => _data['info']['nickname'], 
-          #:avatar => _filename,
-          :email => _raw_info[:email]
-        })
-
-
-        _mem.mem_info.update_attributes({
-          :gender => _data['extra']['gender'],
-          :location=> _raw_info['location'],
-          :html_url=> _raw_info['html_url'],
-          :blog=> _raw_info['blog'],
-          :followers=> _raw_info['followers'],
-          :following=> _raw_info['following'],
-          :github=> _raw_info['login']
-        })
-        Github.new.mem_sync_repo _mem
+        create _data, _provider
       end
       _mem.mauths.create(_para)
       session[:mem] = _mem.id
-      
     else 
       session[:mem] = _mauth.mem.id
     end
     #render json: _data and return
     redirect_to session[:login_callback]
+  end
+
+  def create data,provider
+    _raw_info = data['extra']['raw_info']
+    _photo = ''
+    if provider == 'github'
+      _photo = _raw_info['avatar_url']
+    end
+
+    #require 'uuidtools'   
+    #_filename =  UUIDTools::UUID.timestamp_create.to_s + '.jpg'
+    #upload_remote(_photo,_filename,'mem') 
+    _mem = Mem.create({
+      :nc => data['info']['nickname'], 
+      #:avatar => _filename,
+      :email => _raw_info[:email]
+    })
+
+
+    _mem.mem_info.update_attributes({
+      :gender => data['extra']['gender'],
+      :location=> _raw_info['location'],
+      :html_url=> _raw_info['html_url'],
+      :blog=> _raw_info['blog'],
+      :followers=> _raw_info['followers'],
+      :following=> _raw_info['following'],
+      :github=> _raw_info['login']
+    })
+    Github.mem_sync_repo _mem
+  rescue
   end
 
   def login
