6d42ec428c58a48a95a9641a973de6441caa18ab
unbuntu server <hxh@localhost.(none)>
2015-10-31 22:48:20 -0700

    change repo index from angular to vue

--- app/assets/javascripts/admin/app.js
+++
@@ -1,6 +0,0 @@
-var adminApp = angular.module('adminApp',[])
-
-adminApp.config(['$httpProvider',function($httpProvider) {
-  $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
-}])
-
