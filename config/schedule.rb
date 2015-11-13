f34e18f23d4ae2a728991c4b75b2aa12bfc0199c
hxh1246996371 <1246996371@qq.com>
2015-11-13 20:25:20 +0800

    add sync repo cover to whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,8 +29,13 @@ every 1.day, :at => '1:00 am' do
   rake "mem:avatar"
 end
 
+every 1.day, :at => '3:00 am' do
+  rake "repo:cover"
+end
+
 
 every 4.day, :at => '6:00 am' do
   rake "repo:trend"
 end
 
+# run whenever -w
