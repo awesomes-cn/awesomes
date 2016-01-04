9157f5f62b5899bcbbe6633f7e155d444f011cef
awesomes-cn <13641248047@qq.com>
2016-01-04 13:53:48 +0800

    change svpa font style

--- app/models/mem.rb
+++ app/models/mem.rb
@@ -16,7 +16,7 @@ class Mem < ActiveRecord::Base
   end
 
   def avatar_url
-    avatar.blank? ? "http://svpa.awesomes.cn/avatar/#{nc}?size=100&font=movie" : "#{Rails.application.config.source_access_path}mem/#{avatar}"
+    avatar.blank? ? "http://svpa.awesomes.cn/avatar/#{nc}?size=100&font=kingthings" : "#{Rails.application.config.source_access_path}mem/#{avatar}"
   end
 
   def index
