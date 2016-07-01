c58a10f3986997c4730d5492a5be2f64265811a6
黄小红 <hxh@localhost.(none)>
2016-07-01 12:49:01 +0800

    top100 rank filter nodejs repos

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -2,6 +2,6 @@ class RankController < ApplicationController
   def index
     _map = {:hot => "(stargazers_count + forks_count + subscribers_count)", :trend => "trend"}
     @sort = params[:sort] || 'hot'
-    @items = data_list(Repo.order("#{_map[@sort.to_sym] } desc"), 100)
+    @items = data_list(Repo.where.not({rootyp: "NodeJS"}).order("#{_map[@sort.to_sym] } desc"), 100)
   end
 end
